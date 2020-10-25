c18af292fad85e1acd952442bfffe9cccaca29e6
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-10-25 11:24:56 -0300

    Job configuration - Annotations

--- MigracaoDadosJob/src/main/java/com/springbatch/migracaodados/job/MigracaoDadosJobConfig.java
+++ MigracaoDadosJob/src/main/java/com/springbatch/migracaodados/job/MigracaoDadosJobConfig.java
@@ -2,12 +2,15 @@ package com.springbatch.migracaodados.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
 @Configuration
 public class MigracaoDadosJobConfig {
 	
@@ -16,9 +19,8 @@ public class MigracaoDadosJobConfig {
 	
 	@Bean
 	public Job migracaoDadosjob(
-			Step migrarPessoasStep,
-			Step migrarDadosBancariosStep			
-			) {
+			@Qualifier("migrarPessoasStep") Step migrarPessoasStep,
+			@Qualifier("migrarDadosBancariosStep") Step migrarDadosBancariosStep) {
 		return jobBuilderFactory
 				.get("migracaoDadosjob")
 				.start(migrarPessoasStep)
