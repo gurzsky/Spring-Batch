a677408b7e73bed1a44c506aae63b8481f8d0663
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-11-11 13:39:01 -0300

    Creating a customized scheduler to run the job

---
+++ EnvioPromocoesClientesJob/src/main/java/com/springbatch/enviopromocoesemail/job/EnvioPromocoesClientesScheduleJob.java
@@ -0,0 +1,44 @@
+package com.springbatch.enviopromocoesemail.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class EnvioPromocoesClientesScheduleJob extends QuartzJobBean {
+
+	@Autowired
+	private Job job;
+	
+	@Autowired
+	private JobExplorer jobExplorer;
+	
+	@Autowired
+	private JobLauncher jobLauncher;	
+	
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		
+		JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer)
+				.getNextJobParameters(this.job)
+				.toJobParameters();
+				
+		try {
+			this.jobLauncher.run(this.job, jobParameters);
+		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+				| JobParametersInvalidException e) {
+			e.printStackTrace();
+		}
+				
+	}
+
+}
