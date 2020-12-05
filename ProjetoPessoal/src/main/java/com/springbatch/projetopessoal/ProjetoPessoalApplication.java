8424204a00605e63c3d2f114357585eb036122d4
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-12-05 14:30:47 -0300

    Initial configuration

---
+++ ProjetoPessoal/src/main/java/com/springbatch/projetopessoal/ProjetoPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.projetopessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPessoalApplication.class, args);
+	}
+
+}
