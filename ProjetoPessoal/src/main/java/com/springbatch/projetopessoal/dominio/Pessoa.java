b5ffa5f3d1acf45527a4df3b6114e0feab69d142
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-12-06 15:06:00 -0300

    Reader SQL

--- ProjetoPessoal/src/main/java/com/springbatch/projetopessoal/dominio/Pessoa.java
+++ ProjetoPessoal/src/main/java/com/springbatch/projetopessoal/dominio/Pessoa.java
@@ -1,5 +1,27 @@
 package com.springbatch.projetopessoal.dominio;
 
 public class Pessoa {
-
+	
+	private int id;
+	private String nome;
+	private int idade;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
--- ProjetoPessoal/src/main/java/com/springbatch/projetopessoal/reader/ReaderPessoasConfig.java
+++ ProjetoPessoal/src/main/java/com/springbatch/projetopessoal/reader/ReaderPessoasConfig.java
@@ -6,20 +6,23 @@ import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.springbatch.projetopessoal.dominio.Pessoa;
 
+@Configuration
 public class ReaderPessoasConfig {
 	
 	@Bean
 	public JdbcCursorItemReader<Pessoa> lerPessoas(
 			@Qualifier("appDataSource") DataSource dataSource) {
 		
-		return new JdbcCursorItemReaderBuilder()
+		return new JdbcCursorItemReaderBuilder<Pessoa>()
 				.name("lerPessoas")
 				.dataSource(dataSource)
-				.sql("")
+				.sql("select * from pessoa")
+				.rowMapper(new BeanPropertyRowMapper<Pessoa>(Pessoa.class))
 				.build();
 	}
-
 }
